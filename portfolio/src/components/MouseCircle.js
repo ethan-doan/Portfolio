9614066a0839022574e8ccb435d5f3937c98b7cb
ethandoan <ethan.doan@wilink.de>
2023-12-05 14:22:39 -0600

    Added Mouse Magnet Effect

--- portfolio/src/components/MouseCircle.js
+++ portfolio/src/components/MouseCircle.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/MouseCircle.css";
 
 const MouseCircle = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const mousePosition = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleMouseMove = (e) => {
-      setPosition({
+      mousePosition.current = {
         x: e.clientX,
         y: e.clientY,
-      });
+      };
     };
 
     window.addEventListener("mousemove", handleMouseMove);
@@ -17,6 +18,25 @@ const MouseCircle = () => {
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
+  }, []);
+
+  useEffect(() => {
+    const moveCircle = () => {
+      setPosition((currentPosition) => {
+        const dx = (mousePosition.current.x - currentPosition.x) * 0.075; // Adjust 0.1 for slower/faster movement
+        const dy = (mousePosition.current.y - currentPosition.y) * 0.075; // Adjust 0.1 for slower/faster movement
+        return {
+          x: currentPosition.x + dx,
+          y: currentPosition.y + dy,
+        };
+      });
+
+      requestAnimationFrame(moveCircle);
+    };
+
+    requestAnimationFrame(moveCircle);
+
+    return () => cancelAnimationFrame(moveCircle);
   }, []);
 
   return (
