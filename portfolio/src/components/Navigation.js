78082b8e95df0d3b14075c09d41b5031ffe11c3f
ethandoan <ethan.doan@wilink.de>
2023-12-06 13:03:11 -0600

    Animation Complete

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -4,25 +4,39 @@ import "../styles/Navigation.css";
 
 function Navigation() {
   const [contentHeight, setHeight] = useState(0);
+  const [previewCardPosition, setPreviewCardPosition] = useState("0");
 
   useEffect(() => {
-    // Set navContainer Height
+    // Function to calculate space occupied by the header
     const calculateOccupiedSpace = () => {
       const header = document.getElementsByClassName("header")[0];
-      const headerHeight = header ? header.offsetHeight : 0;
-      return headerHeight;
+      return header ? header.offsetHeight : 0;
     };
 
+    // Function to update height on resize
     const handleResize = () => {
-      setHeight(calculateOccupiedSpace());
+      const newContentHeight = calculateOccupiedSpace();
+
+      setHeight(newContentHeight);
+
+      const windowHeight = window.innerHeight;
+      const newPreviewCardPosition = windowHeight - newContentHeight + "px";
+      setPreviewCardPosition(newPreviewCardPosition);
     };
 
-    handleResize();
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call initially to set height
+
+    // Cleanup listener on component unmount
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const navContainerStyle = {
-    height: `calc(100vh - ${contentHeight + 1}px)`, // Adjust the height dynamically
+    height: `calc(100vh - ${contentHeight}px)`,
+  };
+
+  const previewCardStyle = {
+    top: `calc(${previewCardPosition} - 7.5vh + 1px)`,
   };
 
   return (
@@ -32,37 +46,35 @@ function Navigation() {
           <li className="navItem">
             <span className="altFont">01 </span>
             <span className="navFont">About Me</span>
-            <div className="previewCard">
+            <div className="previewCard" style={previewCardStyle}>
               <p className="previewTitle">A Little Bit About Me</p>
             </div>
           </li>
           <li className="navItem">
             <span className="altFont">02 </span>
             <span className="navFont">Experience</span>
-            <div className="previewCard">
+            <div className="previewCard" style={previewCardStyle}>
               <p className="previewTitle">My Work Experience</p>
             </div>
           </li>
           <li className="navItem">
             <span className="altFont">03 </span>
             <span className="navFont">Education</span>
-            <div className="previewCard">
+            <div className="previewCard" style={previewCardStyle}>
               <p className="previewTitle">Education & Leadership</p>
             </div>
           </li>
           <li className="navItem">
             <span className="altFont">04 </span>
             <span className="navFont">Projects</span>
-            <div className="previewCard">
-              {" "}
+            <div className="previewCard" style={previewCardStyle}>
               <p className="previewTitle">Projects I've Worked On</p>
             </div>
           </li>
           <li className="navItem">
             <span className="altFont">05 </span>
             <span className="navFont">Contact</span>
-            <div className="previewCard">
-              {" "}
+            <div className="previewCard" style={previewCardStyle}>
               <p className="previewTitle">Contact Me</p>
             </div>
           </li>
