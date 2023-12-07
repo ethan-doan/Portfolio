9d01edc809831cf6008d77d66d63ff565d65b9af
ethandoan <ethan.doan@wilink.de>
2023-12-07 12:46:27 -0600

    Preview Card is Fully Responsive

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -3,24 +3,35 @@ import { useState, useEffect } from "react";
 import "../styles/Navigation.css";
 
 function Navigation() {
-  const [contentHeight, setHeight] = useState(0);
+  const [contentHeight, setContentHeight] = useState(0);
   const [previewCardPosition, setPreviewCardPosition] = useState("0");
+  const [activeCard, setActiveCard] = useState(null);
 
   useEffect(() => {
     // Function to calculate space occupied by the header
-    const calculateOccupiedSpace = () => {
+    const calculateHeaderHeight = () => {
       const header = document.getElementsByClassName("header")[0];
       return header ? header.offsetHeight : 0;
     };
 
+    const calculateNavBodyHeight = () => {
+      const nav = document.getElementsByClassName("navBody")[0];
+      return nav ? nav.offsetHeight : 0;
+    };
+
     // Function to update height on resize
     const handleResize = () => {
-      const newContentHeight = calculateOccupiedSpace();
+      const headerHeight = calculateHeaderHeight();
+      const navBodyHeight = calculateNavBodyHeight();
 
-      setHeight(newContentHeight);
+      setContentHeight(headerHeight);
 
       const windowHeight = window.innerHeight;
-      const newPreviewCardPosition = windowHeight - newContentHeight + "px";
+      const newPreviewCardPosition = windowHeight - headerHeight + "px";
+      document.documentElement.style.setProperty(
+        "--card-height",
+        -windowHeight + headerHeight + "px"
+      );
       setPreviewCardPosition(newPreviewCardPosition);
     };
 
@@ -39,42 +50,66 @@ function Navigation() {
     top: `calc(${previewCardPosition} - 7.5vh + 1px)`,
   };
 
+  const handleNavItemClick = (itemIndex) => {
+    setActiveCard(itemIndex === activeCard ? null : itemIndex);
+  };
+
   return (
     <>
       <div className="navContainer" style={navContainerStyle}>
         <ul className="navBody">
-          <li className="navItem">
+          <li className="navItem" onClick={() => handleNavItemClick(0)}>
             <span className="altFont">01 </span>
             <span className="navFont">About Me</span>
-            <div className="previewCard" style={previewCardStyle}>
+            <div
+              className={`previewCard ${activeCard === 0 ? "active" : ""}`}
+              style={previewCardStyle}
+            >
               <p className="previewTitle">A Little Bit About Me</p>
+              {activeCard === 0 && (
+                <p className="previewContent">
+                  Additional information about About Me...
+                </p>
+              )}
             </div>
           </li>
-          <li className="navItem">
+          <li className="navItem" onClick={() => handleNavItemClick(1)}>
             <span className="altFont">02 </span>
             <span className="navFont">Experience</span>
-            <div className="previewCard" style={previewCardStyle}>
+            <div
+              className={`previewCard ${activeCard === 1 ? "active" : ""}`}
+              style={previewCardStyle}
+            >
               <p className="previewTitle">My Work Experience</p>
             </div>
           </li>
-          <li className="navItem">
+          <li className="navItem" onClick={() => handleNavItemClick(2)}>
             <span className="altFont">03 </span>
             <span className="navFont">Education</span>
-            <div className="previewCard" style={previewCardStyle}>
+            <div
+              className={`previewCard ${activeCard === 2 ? "active" : ""}`}
+              style={previewCardStyle}
+            >
               <p className="previewTitle">Education & Leadership</p>
             </div>
           </li>
-          <li className="navItem">
+          <li className="navItem" onClick={() => handleNavItemClick(3)}>
             <span className="altFont">04 </span>
             <span className="navFont">Projects</span>
-            <div className="previewCard" style={previewCardStyle}>
+            <div
+              className={`previewCard ${activeCard === 3 ? "active" : ""}`}
+              style={previewCardStyle}
+            >
               <p className="previewTitle">Projects I've Worked On</p>
             </div>
           </li>
-          <li className="navItem">
+          <li className="navItem" onClick={() => handleNavItemClick(4)}>
             <span className="altFont">05 </span>
             <span className="navFont">Contact</span>
-            <div className="previewCard" style={previewCardStyle}>
+            <div
+              className={`previewCard ${activeCard === 4 ? "active" : ""}`}
+              style={previewCardStyle}
+            >
               <p className="previewTitle">Contact Me</p>
             </div>
           </li>
