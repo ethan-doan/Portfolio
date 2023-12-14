c60f3c56b121335992efa9c158005be4f97087da
ethandoan <ethan.doan@wilink.de>
2023-12-13 22:14:52 -0600

    Landing Page Complete

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,7 +2,15 @@ import React from "react";
 import "../styles/LandingPage.css";
 
 function LandingPage() {
-  return <div>Ethan Doan</div>;
+  return (
+    <div className="landing-container">
+      <div className="landing-title">Hi, I'm Ethan</div>
+      <div className="scroll-container">
+        <div className="scroll-text scroll-text-first">Scroll to Discover</div>
+        <div className="scroll-text scroll-text-second">Scroll to Discover</div>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
