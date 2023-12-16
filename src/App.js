d1550016a23a7fe94fcbccc2ae743bf0c1ef6779
ethan-doan <ethandoan200220@gmail.com>
2023-12-15 20:18:34 -0600

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,63 +1,11 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import LandingPage from "./components/LandingPage.js";
 import MainContent from "./components/MainContent.js";
-import MainLandingContent from "./components/MainLandingContent.js";
-import Navbar from "./components/Navbar.js";
 import "./App.css";
 
 function App() {
-  const [transitionProgress, setTransitionProgress] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = (event) => {
-      // Check if the user is at the top of the page
-      if (window.scrollY <= 1) {
-        // Calculates Transition Progress based on scroll direction
-        const delta = event.deltaY;
-        let newProgress = transitionProgress + delta / 3000;
-
-        // Clamp newProgress between 0 and 1
-        newProgress = Math.min(Math.max(newProgress, 0), 1);
-
-        // Prevent default scrolling if in the middle of the transition
-        if (newProgress > 0 && newProgress < 1) {
-          event.preventDefault();
-        }
-
-        setTransitionProgress(newProgress);
-      }
-    };
-
-    window.addEventListener("wheel", handleScroll, { passive: false });
-    return () => window.removeEventListener("wheel", handleScroll);
-  });
-
   return (
     <div className="app">
-      <div className="content-wrapper">
-        <Navbar />
-        <div
-          className="landing-page"
-          style={{ transform: `scale(${1 - transitionProgress})` }}
-        >
-          <LandingPage />
-        </div>
-        <div
-          className="main-content"
-          style={{
-            overflow: transitionProgress >= 1 ? "auto" : "hidden",
-            position: "relative",
-          }}
-        >
-          <MainLandingContent transitionProgress={transitionProgress} />
-        </div>
-      </div>
-      {transitionProgress >= 1 && (
-        <div className="main-landing-content">
-          <MainContent />
-        </div>
-      )}
+      <MainContent />
     </div>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,59 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import LandingPage from "./LandingPage.js";
+import MainLandingContent from "./MainLandingContent";
+import Navbar from "./Navbar";
 import "../styles/MainContent.css";
 
 function MainContent() {
-  return <div>Yo</div>;
+  const [transitionProgress, setTransitionProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      // Check if the user is at the top of the page
+      if (window.scrollY <= 1) {
+        // Calculates Transition Progress based on scroll direction
+        const delta = event.deltaY;
+        let newProgress = transitionProgress + delta / 3000;
+
+        // Clamp newProgress between 0 and 1
+        newProgress = Math.min(Math.max(newProgress, 0), 1);
+
+        // Prevent default scrolling if in the middle of the transition
+        if (newProgress > 0 && newProgress < 1) {
+          event.preventDefault();
+        }
+
+        setTransitionProgress(newProgress);
+      }
+    };
+
+    window.addEventListener("wheel", handleScroll, { passive: false });
+    return () => window.removeEventListener("wheel", handleScroll);
+  });
+
+  return (
+    <div className="main-content-container">
+      <div className="content-wrapper">
+        <Navbar />
+        <div
+          className="landing-page"
+          style={{ transform: `scale(${1 - transitionProgress})` }}
+        >
+          <LandingPage />
+        </div>
+        <div
+          style={{
+            overflow: transitionProgress >= 1 ? "auto" : "hidden",
+            position: "relative",
+          }}
+        >
+          <MainLandingContent transitionProgress={transitionProgress} />
+        </div>
+      </div>
+      {transitionProgress >= 1 && <div className="hello">Hello</div>}
+    </div>
+  );
 }
 
 export default MainContent;
