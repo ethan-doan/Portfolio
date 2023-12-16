87e0710c734f2a8f1858412bc6d1e82af11abd62
ethan-doan <ethandoan200220@gmail.com>
2023-12-15 18:49:47 -0600

    Responsive MainLandingContent Complete

--- src/components/MainLandingContent.js
+++ src/components/MainLandingContent.js
@@ -16,30 +16,32 @@ function MainLandingContent({ transitionProgress }) {
   return (
     <div className="main-landing-content-container">
       <div className="landing-content">
-        <img
-          className="headshot"
-          src={require("../images/Headshot.jpg")}
-          alt="headshot"
-        />
-        <div className={`description`}>
-          I am a passionate, ambitious, and creative Senior at Creighton
-          University. I am pursuing a Bachelor's in Computer Science and Fintech
-          and am driven to discover the implementations of computer science
-          towards financial markets. I am actively seeking to further my
-          understanding and proficiency in these areas while building
-          relationships with others.
-        </div>
+        <div className="content-wrapper">
+          <img
+            className="headshot"
+            src={require("../images/Headshot.jpg")}
+            alt="headshot"
+          />
+          <div className={`description`}>
+            I am a passionate, ambitious, and creative Senior at Creighton
+            University. I am pursuing a Bachelor's in Computer Science and Fintech
+            and am driven to discover the implementations of computer science
+            towards financial markets. I am actively seeking to further my
+            understanding and proficiency in these areas while building
+            relationships with others.
+          </div>
 
-        {/*
-        <div className="description">
-          I have been fortunate enough to gain experience through multiple
-          internships where I have been able to implement my studies in practice
-          while having opportunities to learn and thrive within new
-          environments. In my spare time I enjoy reading, spending time with
-          friends and family, and consistently trying to learn new things. Visit
-          the remainder of my website to learn more about what I have been up
-          to.
-        </div> */}
+          {/*
+          <div className="description">
+            I have been fortunate enough to gain experience through multiple
+            internships where I have been able to implement my studies in practice
+            while having opportunities to learn and thrive within new
+            environments. In my spare time I enjoy reading, spending time with
+            friends and family, and consistently trying to learn new things. Visit
+            the remainder of my website to learn more about what I have been up
+            to.
+          </div> */}
+        </div>
       </div>
       <div className="scroll-container">
         <div className="explore">
