6d2b3ab7f52dec7219d1c6145b6c3dc4e71a8c52
ethan-doan <ethandoan200220@gmail.com>
2023-12-17 13:30:08 -0600

    Trackpad Functionality Added For LandingPage

--- src/App.js
+++ src/App.js
@@ -6,50 +6,44 @@ import "./App.css";
 
 function App() {
   const [animationProgress, setAnimationProgress] = useState(0);
-  const [animationPlayed, setAnimationPlayed] = useState(false);
+  const [isAnimationComplete, setIsAnimationComplete] = useState(false);
+
+  const handleScroll = (event) => {
+    if (!isAnimationComplete) {
+      event.preventDefault(); // Prevent default scroll during animation
+      const newAnimationProgress = Math.min(
+        window.innerHeight,
+        Math.max(0, animationProgress + event.deltaY)
+      );
+      setAnimationProgress(newAnimationProgress);
+
+      if (newAnimationProgress >= window.innerHeight) {
+        setIsAnimationComplete(true); // Animation complete, enable normal scrolling
+      }
+    }
+  };
 
   useEffect(() => {
-    // Has The Animation Been Played
-    if (animationProgress >= 1 && !animationPlayed) {
-      setAnimationPlayed(true);
+    if (!isAnimationComplete) {
+      window.addEventListener("wheel", handleScroll, { passive: false });
     }
 
-    // Tracking Animation Progress and Handling Scroll
-    if (!animationPlayed) {
-      const handleScroll = (event) => {
-        if (window.scrollY <= 1) {
-          // Calculates Animation Progress
-          const delta = event.deltaY;
-          let progress = animationProgress + delta / 3000;
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [animationProgress, isAnimationComplete]);
 
-          // Make it Binary
-          progress = Math.min(Math.max(progress, 0), 1);
-
-          // Prevent Default Scrolling During Animation
-          if (progress > 0 && progress <= 1) {
-            event.preventDefault();
-          }
-
-          // Setter
-          setAnimationProgress(progress);
-          // { passive: false }
-          return () => {
-            window.removeEventListener("wheel", handleScroll);
-          };
-        }
-      };
-
-      window.addEventListener("wheel", handleScroll);
-    }
-  });
+  const scale = isAnimationComplete
+    ? 0
+    : Math.max(0, 1 - animationProgress / window.innerHeight);
 
   return (
     <div className="app">
-      {!animationPlayed && (
+      {!isAnimationComplete && (
         <div
           className="minimize-point"
           style={{
-            transform: `scale(${1 - animationProgress})`,
+            transform: `scale(${scale})`,
           }}
         >
           <LandingPage />
@@ -57,7 +51,7 @@ function App() {
       )}
       <MainContent
         animationProgress={animationProgress}
-        animationPlayed={animationPlayed}
+        animationPlayed={isAnimationComplete}
       />
     </div>
   );
